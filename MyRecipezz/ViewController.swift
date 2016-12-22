1ea5b340dad730c63a96411eafa215b37d25c410
Majid <majid@Majids-MacBook-Pro.local>
2016-12-22 14:14:54 +0500

    Initial Commit

---
+++ MyRecipezz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyRecipezz
+//
+//  Created by Majid on 22/12/2016.
+//  Copyright © 2016 Majid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
